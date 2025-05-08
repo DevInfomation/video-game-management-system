2277063a032e1e49a20586c34844d7de25b621b5
devinfo <devinformationproject@gmail.com>
2025-05-08 23:24:41 +0200

    adding the image and also just making it the background

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/app.css">
     @vite('resources/css/app.css')
     <title>Document</title>
 </head>
-<body>
-
+<body class="min-h-screen global-wallpaper">
+    
     <header>
         @include('partials.header')
     </header>
-
+    
     <main>
         @yield('content')
     </main>
-
+    
     <footer>
         @include('partials.footer')
     </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-bold underline text-white">
       Hello world!
     </h1>
 @endsection
