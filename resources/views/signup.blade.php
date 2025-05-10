d5b4aa4fb416f49a65d1775e521a49a9461b5abb
devinfo <devinformationproject@gmail.com>
2025-05-10 20:27:33 +0200

    style/fix: shrinking the size, raising the box higher and adding the route

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.default')
 @section('content')
-    <div class="flex justify-center items-center h-screen">
-        <form action="#" method="POST">
+    <div class="flex justify-center items-center h-[80vh]">
+        <form action="{{route('signup.submit')}}" method="POST">
+            @csrf
             <div class="bg-[#473f31] p-3 rounded-t-md">
                 <p class="text-center text-xl text-white">SIGN UP</p>
             </div>
-            <div class="bg-[#b0a485] rounded-1 rounded-b-md p-36">
+            <div class="bg-[#b0a485] rounded-1 rounded-b-md p-15">
                 <div class="flex flex-col">
                     <label class="mb-1">Name:</label>
-                    <input class="border rounded-md p-2 w-[25rem] mb-3" type="text" name="name" placeholder="Enter name here...">
+                    <input class="border rounded-md p-2 w-[25rem] mb-5" type="text" name="name" placeholder="Enter name here...">
                 </div>
                 <div class="flex flex-col">
                     <label class="mb-1">Email:</label>
-                    <input class="border rounded-md p-2 mb-3" type="email" name="email" placeholder="Enter email here...">
+                    <input class="border rounded-md p-2 mb-5" type="text" name="email" placeholder="Enter email here...">
                 </div>
                 <div class="flex flex-col">
                     <label class="mb-1">Password:</label>
                     <input class="border rounded-md p-2" type="password" name="password" placeholder="Enter password here...">
                 </div>
                 <div class="grid grid-cols-1">
-                    <button class="text-white place-self-center bg-[#473f31] mt-4 p-2 pl-10 pr-10">Sign up</button>
+                    <button class="rounded-sm text-white place-self-center bg-[#473f31] mt-8 p-2 pl-10 pr-10 cursor-pointer">Sign up</button>
                 </div>
             </div>
         </form>
