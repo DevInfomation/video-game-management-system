6d027a36c9af3b5f85b67e0466d93987e29df7c0
devinfo <devinformationproject@gmail.com>
2025-05-10 16:37:02 +0200

    style: creating the look of the signup

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.default')
 @section('content')
-    <p class="text-white">test</p>
+    <div class="flex justify-center items-center h-screen">
+        <form action="#" method="POST">
+            <div class="bg-[#473f31] p-3 rounded-t-md">
+                <p class="text-center text-xl text-white">SIGN UP</p>
+            </div>
+            <div class="bg-[#b0a485] rounded-1 p-28 rounded-b-md">
+                <div class="flex flex-col">
+                    <label>Name:</label>
+                    <input type="text" name="name" placeholder="Enter name here...">
+                </div>
+                <div class="flex flex-col">
+                    <label>Email:</label>
+                    <input type="email" name="email" placeholder="Enter name here...">
+                </div>
+                <div class="flex flex-col">
+                    <label>Password:</label>
+                    <input type="password" name="password" placeholder="Enter name here...">
+                </div>
+                <div class="flex flex-col">
+                    <button class="text-white items-center bg-[#473f31] mt-3">Sign up</button>
+                </div>
+            </div>
+        </form>
+    </div>
 @endsection
