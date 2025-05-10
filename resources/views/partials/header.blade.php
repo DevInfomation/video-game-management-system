f501a4494e0fd794127e21a3283a8f395e0ca68a
devinfo <devinformationproject@gmail.com>
2025-05-10 20:26:27 +0200

    feat: adding the dasboard to the a href

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 @unless (Auth::check()) {{-- Check to see if the user is logged in --}}
     <div class="grid grid-cols-2 border rounded-1 bg-black p-2">
         <div class="w-1/2">
-            <a href="#"><h2 class="text-white text-2xl ml-2 mt-1">Video Game Management System</h2></a>
+            <a href="/dashboard"><h2 class="text-white text-2xl ml-2 mt-1">Video Game Management System</h2></a>
         </div>
         <div class="flex justify-end">
             <a href="/login"><p class="text-white text-lg mt-1 mr-8">Login</p></a>     
@@ -11,7 +11,7 @@
 @else
     <div class="grid grid-cols-3 border rounded-1 bg-black p-2">
         <div class="w-1/2">
-            <a href="#"><h2 class="text-white text-2xl ml-2 mt-1">Video Game Management System</h2></a>
+            <a href="/dashboard"><h2 class="text-white text-2xl ml-2 mt-1">Video Game Management System</h2></a>
         </div>
     </div
 
