dcdb966f94b37f7c4c2da278e59acd5e1b068f3f
devinfo <devinformationproject@gmail.com>
2025-05-10 15:06:19 +0200

    feat: adding a hrefs to the login and sign ups and title

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,18 @@
-@unless (Auth::check())
+@unless (Auth::check()) {{-- Check to see if the user is logged in --}}
     <div class="grid grid-cols-2 border rounded-1 bg-black p-2">
         <div class="w-1/2">
             <a href="#"><h2 class="text-white text-2xl ml-2 mt-1">Video Game Management System</h2></a>
         </div>
         <div class="flex justify-end">
-            <p class="text-white text-lg mt-1 mr-8">Login</p>     
-            <p class="text-white text-lg mt-1">Signup</p>     
+            <a href="#"><p class="text-white text-lg mt-1 mr-8">Login</p></a>     
+            <a href="#"><p class="text-white text-lg mt-1">Signup</p></a>    
         </div> 
     </div>
 @else
-
+    <div class="grid grid-cols-3 border rounded-1 bg-black p-2">
+        <div class="w-1/2">
+            <a href="#"><h2 class="text-white text-2xl ml-2 mt-1">Video Game Management System</h2></a>
+        </div>
+    </div
 
 @endunless
