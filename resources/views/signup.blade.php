89b00d9fb250d9a66e9e6fbe6db087cc539406f3
devinfo <devinformationproject@gmail.com>
2025-05-10 16:52:26 +0200

    style: making the box bigger and giving the inputs borders

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -2,24 +2,24 @@
 @section('content')
     <div class="flex justify-center items-center h-screen">
         <form action="#" method="POST">
-            <div class="bg-[#473f31] p-3 rounded-t-md">
+            <div class="bg-[#473f31] p-3 rounded-t-md p-2">
                 <p class="text-center text-xl text-white">SIGN UP</p>
             </div>
-            <div class="bg-[#b0a485] rounded-1 p-28 rounded-b-md">
+            <div class="bg-[#b0a485] rounded-1 rounded-b-md p-36">
                 <div class="flex flex-col">
-                    <label>Name:</label>
-                    <input type="text" name="name" placeholder="Enter name here...">
+                    <label class="mb-1">Name:</label>
+                    <input class="border rounded-md p-2 w-[25rem] mb-3" type="text" name="name" placeholder="Enter name here...">
                 </div>
                 <div class="flex flex-col">
-                    <label>Email:</label>
-                    <input type="email" name="email" placeholder="Enter name here...">
+                    <label class="mb-1">Email:</label>
+                    <input class="border rounded-md p-2 mb-3" type="email" name="email" placeholder="Enter email here...">
                 </div>
                 <div class="flex flex-col">
-                    <label>Password:</label>
-                    <input type="password" name="password" placeholder="Enter name here...">
+                    <label class="mb-1">Password:</label>
+                    <input class="border rounded-md p-2" type="password" name="password" placeholder="Enter password here...">
                 </div>
                 <div class="flex flex-col">
-                    <button class="text-white items-center bg-[#473f31] mt-3">Sign up</button>
+                    <button class="text-white items-center bg-[#473f31] mt-3 p-2">Sign up</button>
                 </div>
             </div>
         </form>
