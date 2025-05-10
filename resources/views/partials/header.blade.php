1838df6286de7855193e286da72d024be4a6be7b
devinfo <devinformationproject@gmail.com>
2025-05-10 20:46:37 +0200

    style: adding the name and design to the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,9 +10,18 @@
     </div>
 @else
     <div class="grid grid-cols-3 border rounded-1 bg-black p-2">
-        <div class="w-1/2">
+        <div class="w-full">
             <a href="/dashboard"><h2 class="text-white text-2xl ml-2 mt-1">Video Game Management System</h2></a>
         </div>
+        <div class="flex justify-end">
+            <a href="/login"><p class="text-white text-lg mt-1 mr-8">Welcome: {{$user->name}}</p></a>     
+            <i class="fa-solid fa-cart-shopping text-white text-xl mt-2"></i>
+        </div> 
+        <div class="flex justify-end">
+            <form action="#" method="POST">
+                @csrf
+                <button class="text-white text-lg mt-1 mr-8 cursor-pointer">Logout</button>
+            </form>
+        </div> 
     </div
-
 @endunless
