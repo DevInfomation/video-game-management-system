f0e27aa37db8bae073358df9af7c8a7d054093b1
devinfo <devinformationproject@gmail.com>
2025-05-25 23:19:13 +0200

    feat: adding games to the database

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Game;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'developer' => 'required',
+            'publisher' => 'required',
+            'category' => 'required',
+            'published_year' => 'required',
+            'price' => 'required',
+        ]);
+
+        Game::create($incomingFields);
+        return redirect('/dashboard');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Game $game)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Game $game)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Game $game)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Game $game)
+    {
+        //
+    }
+}
