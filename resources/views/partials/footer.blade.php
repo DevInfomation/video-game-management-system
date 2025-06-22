6317f5ba07c8861588995fa37663e9e2010add64
devinfo <devinformationproject@gmail.com>
2025-06-22 21:22:02 +0200

    style: creating the footer and the bottom footer as well

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,16 +1,47 @@
-<div class="container px-12 flex bg-footer mt-10">
+<div class="px-12 py-3 flex justify-evenly bg-footer mt-10">
     <div class="w-150">
         <h1 class="text-white text-2xl">More About our Website</h1>
         <p class="text-white">At VGMS, we don’t just make games — we create unforgettable journeys that inspire, challenge, and entertain players around the world.</p>
-        <p class="text-white text-right"><i>- Devin Information</i></p>
+        <p class="text-white text-right"><i>- Devinformation</i></p>
     </div>
-    <div>
+    <div class="px-8">
         <h1 class="text-white text-2xl">Keep Connected</h1>
         <div>
-            <i class="fa-brands fa-facebook"></i>
-            <p>Follow us on Facebook</p>
-            <i class="fa-brands fa-twitter"></i>
-            <p>Follow us on Twitter</p>
+            <div class="flex">
+                <i class="fa-brands fa-facebook mt-1 mr-1" style="color: #ffffff;"></i>
+                <p class="text-white">Follow us on Facebook</p>
+            </div>
+            <div class="flex">
+                <i class="fa-brands fa-twitter mt-1 mr-1" style="color: #ffffff;"></i>
+                <p class="text-white">Follow us on Twitter</p>
+            </div>
         </div>
+    </div>
+    <div class="px-8">
+        <h1 class="text-white text-2xl">Contact Information</h1>
+        <div>
+            <div class="flex">
+                <i class="fa-solid fa-house mt-1 mr-1" style="color: #ffffff;"></i>
+                <p class="text-white">Skopje, Macedonia</p>
+            </div>
+            <div class="flex">
+                <i class="fa-solid fa-phone mt-1 mr-1" style="color: #ffffff;"></i>
+                <p class="text-white">+389-725-559-2217</p>
+            </div>
+            <div class="flex">
+                <i class="fa-solid fa-envelope mt-1 mr-1" style="color: #ffffff;"></i>
+                <p class="text-white">randomrandom@gmail.com</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bg-footer-bottom flex px-12 py-3 justify-between">
+    <div>
+        <p class="text-white">&copy; 2025 VGMS, all rights reserved | designed by me</p>
+    </div>
+    <div class="flex">
+        <p class="text-white">Company Information |</p>
+        <p class="text-white ml-1">Privacy Policy |</p>
+        <p class="text-white ml-1">Terms & Condition</p>
     </div>
 </div>
