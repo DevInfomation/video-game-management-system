a08c8d8aad8dc0b909b311fcffb72445c2ff7388
devinfo <devinformationproject@gmail.com>
2025-06-22 17:48:53 +0200

    style: adding a footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,16 @@
-{{-- <div class="border rounded-1 bg-black p-2">
-    <p class="text-white">test</p>
-</div --}}
+<div class="container px-12 flex bg-footer mt-10">
+    <div class="w-150">
+        <h1 class="text-white text-2xl">More About our Website</h1>
+        <p class="text-white">At VGMS, we don’t just make games — we create unforgettable journeys that inspire, challenge, and entertain players around the world.</p>
+        <p class="text-white text-right"><i>- Devin Information</i></p>
+    </div>
+    <div>
+        <h1 class="text-white text-2xl">Keep Connected</h1>
+        <div>
+            <i class="fa-brands fa-facebook"></i>
+            <p>Follow us on Facebook</p>
+            <i class="fa-brands fa-twitter"></i>
+            <p>Follow us on Twitter</p>
+        </div>
+    </div>
+</div>
