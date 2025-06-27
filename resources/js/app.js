e70df0ddf23e7dd8c58ee5b72ab050f2f3732c33
devinfo <devinformationproject@gmail.com>
2025-06-27 15:56:33 +0200

    feat: same with this one. Added in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+let accordions = document.querySelectorAll('.accordion-wrapper .accordion');
+
+accordions.forEach((accordion) => {
+    accordion.onclick = () => {
+        accordions.forEach((subcontent) => {
+            subcontent.classList.remove('active')
+        });
+        accordion.classList.add('active');
+    }
+});
+    
